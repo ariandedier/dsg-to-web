41b6903ed239d638ea0ca3b2d0bce77e33ae04b5
ariandedier <ariandedierfea@gmail.com>
2022-04-20 11:32:48 +0100

    Main page and form completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import styled from "styled-components";
 import Header from './components/Header'
+import Main from "./components/Main";
 
 function App() {
   return (
     <Container>
       <Header/>
+      <Main/>
     </Container>
   );
 }
 
 const Container = styled.div`
-
+  display: flex;
+  flex-direction: column;
 `
 
 export default App;
